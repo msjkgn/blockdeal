2dcfc7bc3f10278999b6a54e7d19c703a863b289
MSJ <>
2022-07-17 18:14:02 +0900

    new contract

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -78,14 +78,14 @@ export function useContract<T extends Contract = Contract>(
 export function useFactoryContract() {
   const { library, account, chainId } = useActiveWeb3React() // web3 react
   if (!library || !chainId) return null
-  return getContract('0xb46c002e68de024b982b6ca76cb2e36ce704f05f', FACTORY_ABI, library, account ? account : undefined)
+  return getContract('0x2C31c67114A51e0e2393A3fA6337720C2bD51b26', FACTORY_ABI, library, account ? account : undefined)
 }
 
 export function usePairContract2() {
   const { library, account, chainId } = useActiveWeb3React() // web3 react
   if (!library || !chainId) return null
   // TODO: ADD ETH/USDT Pair address after creating from factory.
-  return getContract('0xB34b0C29F48fe499d19fB49b7B7fA92576E70BEF', PAIR_ABI, library, account ? account : undefined)
+  return getContract('0xde6de9e653b0a1c321e84e7512dab99b8d383c92', PAIR_ABI, library, account ? account : undefined)
 }
 
 export function useWETHTest() {
